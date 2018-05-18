124cca399764941c3bf0ff3960cacf2f74cbce00
Grygoriy Zolotarov <zolotaryovgl@gmail.com>
2018-05-18 12:05:03 +0200

    first commmit

---
+++ GSEA.R
@@ -0,0 +1,222 @@
+#Here the GSEA and GAGE are performed using GO,KEGG and Rectome gene sets
+rm(list=ls()) 
+library(ReactomePA)
+biocLite('reactome.db')
+library(BiocInstaller)
+#1.----Load packages----
+list.of.packages <- c("ggplot2",
+                      'clusterProfiler',
+                      'ReactomePA',
+                      'DOSE',
+                      'bindrcpp',
+                      'ggplotly',
+                      'dplyr',
+                      'org.Mm.eg.db')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+if(length(new.packages)) biocLite(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+rm(list = c('list.of.packages','new.packages'))
+
+#1.----Load DESeq2 results ----
+setwd("~/interferon_gamma/DESeq2_results")
+files = list.files(pattern = 'csv')
+read.file = function(i){
+  result = read.csv(files[i])
+  name = unlist(strsplit(files[i],'\\.'))[1]
+  comment(result) =readLines(list.files(pattern = paste0(unlist(strsplit(name,'_'))[1],'_info')))
+  return(result)
+}
+desseq2 = lapply(seq_along(files),FUN = read.file)
+names(desseq2) = sapply(files,function(x) unlist(strsplit(x,'_'))[1],USE.NAMES = F)
+
+#2.----Define functions----
+compute_group_go = function(sign.genes,level=1){
+  list = list()
+  ont = c('BP','MF','CC')
+  for(i in 1:length(ont)){
+    message(paste0('groupGO...',ont[i]))
+    start_time <- Sys.time()
+    ggo<- groupGO(gene     = sign.genes,
+                  OrgDb    = org.Mm.eg.db,
+                  ont      = ont[i],
+                  level    = level,
+                  readable = TRUE)
+    list[ont[i]] = ggo
+    end_time <- Sys.time()
+    print(end_time - start_time)
+  }
+  return(list)
+}
+
+compute_enrich_go = function(sign.genes,level=1){
+  list = list()
+  ont = c('BP','MF','CC')
+  for(i in 1:length(ont)){
+    message(paste0('groupGO...',ont[i]))
+    start_time <- Sys.time()
+    ggo<- enrichGO(gene     = sign.genes,
+                   OrgDb    = org.Mm.eg.db,
+                   ont      = ont[i],
+                   readable = TRUE)
+    list[ont[i]] = ggo
+    end_time <- Sys.time()
+    print(end_time - start_time)
+  }
+  return(list)
+}
+compute_gsea_go = function(genes,ont = ont,pval = 0.05){
+  list = list()
+  ont = c('BP','MF','CC')
+  for(i in 1:length(ont)){
+    message(paste0('gseaGO...',ont[i]))
+    start_time <- Sys.time()
+    gse<- gseGO(geneList     = genes,
+                OrgDb        = org.Mm.eg.db,
+                ont          = ont[i],
+                nPerm        = 1000,
+                minGSSize    = 10,
+                maxGSSize    = 2000,
+                pvalueCutoff = pval,
+                verbose      = T)
+    list[ont[i]] = gse
+    end_time <- Sys.time()
+    print(end_time - start_time)
+  }
+  return(list)
+}
+#2.----Gene Enrichment and GSEA: clusterProfiler----
+#----Cluster Profiler ----
+#----GO enrichment----
+enrich_go = list()
+for(i in 1:length(names(desseq2))){
+  print(names(desseq2)[i])
+  #list of signifint genes and data
+  result = desseq2[[i]]
+  genes = result$log2FoldChange
+  names(genes) = as.character(result$entrez)
+  genes = sort(na.omit(genes))
+  genes = sort(genes[!is.na(names(genes))],decreasing = T)
+  sign.genes = result%>%
+    filter(abs(log2FoldChange)>=1&padj<=0.01)
+  sign.genes = as.character(sign.genes$entrez)
+  enrich_go[[i]] = compute_enrich_go(sign.genes = sign.genes)
+  names(enrich_go)[i] = names(desseq2)[i]
+}
+names(enrich_go) = unlist(strsplit(names(enrich_go),'_'))[seq(1,length(names(enrich_go))*2,2)]
+#----GO gsea----
+gsea_go = list()
+for(i in 1:length(names(desseq2))){
+  print(names(desseq2)[i])
+  #list of signifint genes and data
+  result = desseq2[[i]]
+  genes = result$log2FoldChange
+  names(genes) = as.character(result$entrez)
+  genes = sort(na.omit(genes))
+  genes = sort(genes[!is.na(names(genes))],decreasing = T)
+  sign.genes = result%>%
+    filter(abs(log2FoldChange)>=1&padj<=0.01)
+  sign.genes = as.character(sign.genes$entrez)
+  gsea_go[i] = compute_gsea_go(genes = genes, ont = ont)
+  names(gsea_go)[i] = names(desseq2)[i]
+}
+names(gsea_go) = unlist(strsplit(names(desseq2),'_'))[seq(1,length(names(desseq2))*2,2)]
+#----KEGG enrichment----
+enrich_kegg = list()
+for(i in 1:length(names(desseq2))){
+  print(names(desseq2)[i])
+  #list of signifint genes and data
+  result = desseq2[[i]]
+  genes = result$log2FoldChange
+  names(genes) = as.character(result$entrez)
+  genes = sort(na.omit(genes),decreasing = T)
+  genes = sort(genes[!is.na(names(genes))],decreasing = T)
+  sign.genes = result%>%
+    filter(abs(log2FoldChange)>=1&padj<=0.01)
+  sign.genes = as.character(sign.genes$entrez)
+  enrich_kegg[i] = enrichKEGG(gene    = sign.genes,
+                         organism     = 'mmu',
+                         keyType = 'ncbi-geneid',
+                         minGSSize    = 10,
+                         pvalueCutoff = 0.05)
+  names(enrich_kegg)[i] = names(desseq2)[i]
+}
+names(enrich_kegg) = unlist(strsplit(names(desseq2),'_'))[seq(1,length(names(desseq2))*2,2)]
+#----KEGG gsea----
+gsea_kegg = list()
+for(i in 1:length(names(desseq2))){
+  print(names(desseq2)[i])
+  #list of signifint genes and ata
+  result = desseq2[[i]]
+  genes = result$log2FoldChange
+  names(genes) = as.character(result$entrez)
+  genes = sort(na.omit(genes),decreasing = T)
+  genes = sort(genes[!is.na(names(genes))],decreasing = T)
+  sign.genes = result%>%
+    filter(abs(log2FoldChange)>=1&padj<=0.01)
+  sign.genes = as.character(sign.genes$entrez)
+  gsea_kegg[i] = gseKEGG(geneList     = genes,
+                         organism     = 'mmu',
+                         nPerm        = 3000,
+                         keyType = 'ncbi-geneid',
+                         minGSSize    = 10,
+                         pvalueCutoff = 0.05,
+                         verbose      = T)
+  names(gsea_kegg)[i] = names(desseq2)[i]
+}
+names(gsea_kegg) = unlist(strsplit(names(desseq2),'_'))[seq(1,length(names(desseq2))*2,2)]
+#----Reactome enrichment----
+enrich_reactome = list()
+for(i in 1:length(names(desseq2))){
+  print(names(desseq2)[i])
+  #list of signifint genes and data
+  result = desseq2[[i]]
+  genes = result$log2FoldChange
+  names(genes) = as.character(result$entrez)
+  genes = sort(na.omit(genes),decreasing = T)
+  genes = sort(genes[!is.na(names(genes))],decreasing = T)
+  sign.genes = result%>%
+    filter(abs(log2FoldChange)>=1&padj<=0.01)
+  sign.genes = na.omit(sign.genes$entrez)
+  enrich_reactome[i] = enrichPathway(sign.genes, pvalueCutoff=0.05,organism = 'mouse',
+                                   pAdjustMethod="BH", readable=T)
+  names(enrich_reactome)[i] = names(desseq2)[i]
+}
+
+#----Reactome gsea----
+gsea_reactome = list()
+for(i in 1:length(names(desseq2))){
+  print(names(desseq2)[i])
+  #list of signifint genes and data
+  result = desseq2[[i]]
+  genes = result$log2FoldChange
+  names(genes) = as.character(result$entrez)
+  genes = sort(na.omit(genes),decreasing = T)
+  genes = sort(genes[!is.na(names(genes))],decreasing = T)
+  sign.genes = result%>%
+    filter(abs(log2FoldChange)>=1&padj<=0.01)
+  sign.genes = as.character(sign.genes$entrez)
+  gsea_reactome[i] = gsePathway(genes, nPerm=3000, organism = 'mouse',
+                                  minGSSize=30, pvalueCutoff=0.05,
+                                  pAdjustMethod="BH", verbose=T)
+  names(gsea_reactome)[i] = names(desseq2)[i]
+}
+names(gsea_reactome) = unlist(strsplit(names(desseq2),'_'))[seq(1,length(names(desseq2))*2,2)]
+
+#3.----Export Results----
+setwd("~/interferon_gamma/gsea")
+
+#GO
+lapply(seq_along(enrich_go),FUN = function(j) lapply(seq_along(enrich_go[[j]]),FUN = function(i) write.csv(enrich_go[[j]][i],paste0(names(enrich_go)[j],'_',names(enrich_go[[j]][i]),'_enrich_go.csv'))))
+#KEGG
+lapply(seq_along(enrich_kegg),FUN = function(i) write.csv(enrich_kegg[i],paste0(names(enrich_kegg[i]),'enrich_kegg.csv')))
+lapply(seq_along(gsea_kegg),FUN = function(i) write.csv(gsea_kegg[i],paste0(names(gsea_kegg[i]),'gsea_kegg.csv')))
+#Reactome
+lapply(seq_along(enrich_reactome),FUN = function(i) write.csv(enrich_reactome[i],paste0(names(enrich_reactome[i]),'enrich_reactome.csv')))
+lapply(seq_along(gsea_reactome),FUN = function(i) write.csv(gsea_reactome[i],paste0(names(gsea_reactome[i]),'gsea_reactome.csv')))
+
+
+
+
+
+
---
+++ counts_annotation.R
@@ -0,0 +1,135 @@
+#Install and load packages
+library('rstudioapi') 
+library('dplyr')
+#1.----Loading packages -----
+rm(list=ls())
+list.of.packages <- c('DESeq2',
+                      'BiocInstaller',
+                      'rstudioapi',
+                      "AnnotationDbi",
+                      "org.Mm.eg.db")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+if(length(new.packages)) biocLite(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+
+
+#2.----Data import -----
+## read counts
+current_path <- getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+setwd('data')
+files.list<- list.files(pattern = 'Sample') #importing list of files
+
+readfile = function(x){
+  df<-read.csv(x,sep='\t',header = F)
+  df<-df[,-c(2,3)]
+  df<-df[-c(1,2,3,4),]
+  colnames(df)<-c('gene',substr(x,1,8))
+  return(df)
+}
+# let's define a function to read owr countdata for each sample and them apply it
+list = lapply(files.list,FUN = readfile)
+data = Reduce(function(x, y) merge(x, y, by = "gene", all = TRUE), list)
+rownames(data)=data$gene
+data = data[,-1]
+
+##reading the metadata
+metadat = read.csv("metadat.csv",sep = ';')
+if(!all(metadat$sample_name == colnames(data))){
+  message('Sample names and column names in metadat.csv are not matching!')
+}else{message('Sample names and column names in metadat.csv are matching :)')}
+
+
+#3.----Analysis----
+## Preparing input for DESeq2 analysis
+#----Differential expression analysis IFNG ----
+#Create two groups ignoring the cell line information
+dds <- DESeqDataSetFromMatrix(countData = data,
+                              colData = metadat[,-2],
+                              design = ~treatment)
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+dds.ifng <- DESeq(dds)
+IFNG.result <- results(dds.ifng, contrast=c("treatment",'ifng',"control"))
+IFNG.result = na.omit(IFNG.result) #omittnig NAs
+dds <- DESeqDataSetFromMatrix(countData = data,
+                              colData = metadat,
+                              design = ~cell_line+treatment+cell_line:treatment)
+#filtering low-abundant transcripts that have < than 10 transcripts total
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,] 
+nrow(dds) # 20321 genes occured more 10 and more times for all samples
+#----Differential expression analysis for cell lines----
+#estimate the log2fc of genes for each cell line separately
+dds <- DESeq(dds)
+#-----B16 effect
+#main effect
+B16.result <- results(dds, contrast=c("treatment",'ifng',"control"))
+#DESeq2 uses b16 cell line as a base level, so there is no need in adding an interaction coefficient
+B16.result = na.omit(B16.result) #omittnig NAs
+
+#----TC1A9----
+# the condition effect for tc1a9 line
+# this is, by definition, the main effect *plus* the interaction term
+# I've checked the results provided by comparison using a dummy 'group' variable, the amount of significant genes is the same for both lines.
+TC1A9.result <- results(dds, list( c("treatment_ifng_vs_control","cell_lineTC1A9.treatmentifng") ))
+TC1A9.result = na.omit(TC1A9.result)
+#4.----Annotation----
+results = list(B16=B16.result,TC1A9=TC1A9.result,IFNG = IFNG.result)
+for(i in 1:length(results)){
+  print(names(results)[i])
+  res = results[[i]]
+  res$symbol <- mapIds(org.Mm.eg.db,
+                       keys=row.names(res),
+                       column="SYMBOL",
+                       keytype="ENSEMBL",
+                       multiVals="first")
+  res$entrez <- mapIds(org.Mm.eg.db,
+                       keys=row.names(res),
+                       column="ENTREZID",
+                       keytype="ENSEMBL",
+                       multiVals="first")
+  res$ensembl = rownames(res)
+  results[[i]]=res
+  
+}
+
+annotate_genes = function(res){
+  res$symbol <- mapIds(org.Mm.eg.db,
+                       keys=row.names(res),
+                       column="SYMBOL",
+                       keytype="ENSEMBL",
+                       multiVals="first")
+  res$entrez <- mapIds(org.Mm.eg.db,
+                       keys=row.names(res),
+                       column="ENTREZID",
+                       keytype="ENSEMBL",
+                       multiVals="first")
+  res$ensembl = rownames(res)
+  return(res)
+}
+
+results = lapply(results,annotate_genes)
+
+#5.----Export----
+setwd("../")
+if (file.exists('DESeq2_results')){
+  setwd('DESeq2_results')
+  message('DESeq2_results directory already exists, settig wd...')
+}else{
+  dir.create('DESeq2_results')
+  message('creating DESeq2_results directory, settig wd...')
+  setwd('DESeq2_results')
+}
+for(i in 1:length(results)){
+  names(results)[i]
+  write.csv(results[[i]],paste0(names(results)[i],'_result.csv'),row.names = F)
+  file = file(paste0(names(results)[i],"_info.txt")  )  
+  writeLines(c(paste0(names(results)[i],' information:'),
+               results[[i]]@elementMetadata$description[1:6]),
+             file)
+  close(file)
+}
+#exporting results info
+writeLines(c(paste0('Created:'),capture.output(Sys.time()),capture.output(sessionInfo())), "sessionInfo.txt")
